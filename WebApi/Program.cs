df65eddae1947df79a754eecbc00f139db17d818
Irfan Alamsyah <88070126+irfanalmsyah@users.noreply.github.com>
2024-05-29 23:31:40 +0700

    remove swagger

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -47,17 +47,12 @@ builder.Services.AddAuthentication(x =>
 });
 
 builder.Services.AddAuthorization();
-
-// Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(Secret.ConnectionString));
 
 var app = builder.Build();
 
-//check connection to the db
 CheckDatabaseConnection(app.Services);
 
 async Task InitializeDatabaseAsync(IServiceProvider serviceProvider)
@@ -85,15 +80,6 @@ async Task InitializeDatabaseAsync(IServiceProvider serviceProvider)
 }
 
 await InitializeDatabaseAsync(app.Services);
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
